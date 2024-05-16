2520d93c8896a7f6d44b29d63170748245ef8061
farhnss <farhnss@student.telkomuniversity.ac.id>
2024-05-16 19:10:36 +0700

    EDORM-57 Revisi Front End Admin

--- app/Filament/Resources/RoomResource.php
+++ app/Filament/Resources/RoomResource.php
@@ -9,6 +9,7 @@ use Filament\Resources\Form;
 use Filament\Resources\Table;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
@@ -21,6 +22,7 @@ class RoomResource extends Resource
     protected static ?string $model = Room::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
+    
 
     public static function form(Form $form): Form
     {
@@ -28,12 +30,76 @@ class RoomResource extends Resource
             ->schema([
                 Card::make()
                 ->schema([
-                    TextInput::make('penghuni')->unique(ignorable: fn($record) => $record),
-                    TextInput::make('gender'),
-                    TextInput::make('gedung'),
-                    TextInput::make('lantai'),
-                    TextInput::make('kamar'),
+                    Select::make('penghuni')->unique(ignorable: fn($record) => $record),
+                    Select::make('gender')->options([
+                        'Putra' => 'Putra',
+                        'Putri'=> 'Putri',
                     ])
+                    ->reactive(),
+                    Select::make('gedung')
+                    ->options(function (callable $get) {
+                        $gender = $get('gender');
+                        if ($gender === 'Putra') {
+                            return array_combine(
+                            ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09','A10'],
+                            ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09','A10'],
+                            );
+                        } else {
+                            return array_combine(
+                            ['A11', 'A12', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06'],
+                            ['A11', 'A12', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06']
+                            );
+                        }
+                    })
+                    ->afterStateUpdated(function ($state, $get, $set) {
+                        $gender = $get('gender');
+                        if ($gender === 'Putra') {
+                            $set('gedung', array_combine(
+                            ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09','A10'],
+                            ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09','A10'],
+                            ));
+                        } else {
+                            $set('gedung', array_combine(
+                            ['A11', 'A12', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06'],
+                            ['A11', 'A12', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06']
+                            ));
+                        }
+                    }),
+                    Select::make('lantai')->options([1,2,3,4])
+                    ->reactive(),
+                    Select::make('kamar')
+                    ->options(function (callable $get) {
+                        $lantai = $get('lantai');
+                        switch ($lantai) {
+                            case 0:
+                                return array_combine(range(101, 124), range(101,124));
+                            case 1:
+                                return array_combine(range(201, 224), range(201,224));
+                            case 2:
+                                return array_combine(range(301, 324), range(301,324));
+                            case 3:
+                                return array_combine(range(401, 424), range(401,424));
+                            default:
+                                return [];
+                        }
+                    })
+                    ->afterStateUpdated(function ($state, $get, $set) {
+                        $lantai = $get('lantai');
+                        switch ($lantai) {
+                            case 1:
+                                $set('kamar', array_combine([101, 102, 103, 104, 105], [101, 102, 103, 104, 105]));
+                                break;
+                            case 2:
+                                $set('kamar', array_combine([201, 202, 203, 204, 205], [201, 202, 203, 204, 205]));
+                                break;
+                            case 3:
+                                $set('kamar', array_combine([301, 302, 303, 304], [301, 302, 303, 304]));
+                                break;
+                            default:
+                                $set('kamar', []);
+                        }
+                    }),
+            ])
             ]);
     }
 
